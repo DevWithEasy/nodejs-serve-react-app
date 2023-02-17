d62fa5d062b73e537824dfe20c6e5edd8d2b2100
DevWithEasy <devwitheasy@gmail.com>
2023-02-17 18:58:15 +0600

    routing add

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,21 @@ import Faq from './pages/Faq';
 import Home from './pages/Home';
 
 function App() {
-  const route = useRoutes([
-    {path : '/',
-    element : <Home/>,
-    children : [
-      {
-        index : true,
-        element : <Home/>,
-      },
-      {
-        path : '/about',
-        element : <About/>,
-      },
-      {
-        path : '/faq',
-        element : <Faq/>,
-      },
-    ]
-  }
-  ])
-  return route
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />
+    },
+    {
+      path: "/faq",
+      element: <Faq />
+    },
+  ]);
+  return routes;
 }
 
 export default App;
